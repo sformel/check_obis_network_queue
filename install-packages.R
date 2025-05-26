e830938de4d1000e38edcc62d87df625fd1d4cbc
sformel <steve@formeldataservices.com>
2025-05-26 11:12:51 -0500

    change installation and add caching

--- install-packages.R
+++
@@ -1,8 +0,0 @@
-install.packages(c(
-  "httr",       # for GitHub API
-  "jsonlite",   # for parsing JSON
-  "stringr",    # for regex extraction
-  "dplyr",      # for data wrangling
-  "stringdist", # for fuzzy matching
-  "httr2"       # for OBIS API
-))
